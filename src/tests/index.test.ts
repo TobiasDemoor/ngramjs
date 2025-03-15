d19284b06823054d7ce65b90e2e4b75986a0690a
TobiasDemoor <tobias.demoor@hotmail.com>
2025-03-15 12:45:16 -0300

    feat: add coverage

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,45 +1,84 @@
 import { ngram, NgramStyle, tokenize } from "../index";
 
 describe("Test", () => {
-  it("`tokenize` returns list of words", () => {
-    expect(tokenize("")).toEqual([]);
-    expect(tokenize(null as unknown as string)).toEqual([]);
+  describe("tokenize returns list of words", () => {
+    it("should return empty array", () => {
+      expect(tokenize("")).toEqual([]);
+      expect(tokenize(null as unknown as string)).toEqual([]);
+    });
 
-    expect(tokenize("I like opensource!")).toEqual(["I", "like", "opensource"]);
-    expect(tokenize("Anže and špela are tokenized!")).toEqual([
-      "Anže",
-      "and",
-      "špela",
-      "are",
-      "tokenized",
-    ]);
+    it("should return list of words", () => {
+      expect(tokenize("I like opensource!")).toEqual([
+        "I",
+        "like",
+        "opensource",
+      ]);
+      expect(tokenize("Anže and špela are tokenized!")).toEqual([
+        "Anže",
+        "and",
+        "špela",
+        "are",
+        "tokenized",
+      ]);
+    });
   });
 
-  it("`ngram` returns list of grams", () => {
-    expect(ngram("")).toEqual([]);
+  describe("ngram returns list of grams", () => {
+    it("should return empty array", () => {
+      expect(ngram("")).toEqual([]);
+      expect(ngram(null as unknown as string)).toEqual([]);
+    });
 
-    expect(
-      ngram("opensource", { style: NgramStyle.START, min: 3, max: 5 }),
-    ).toEqual(["ope", "open", "opens"]);
-    expect(ngram("foo", { style: NgramStyle.START, min: 3, max: 5 })).toEqual([
-      "foo",
-    ]);
+    it("should set default values for min, max and/or style", () => {
+      // set's style=middle min=0 max=length by default
+      expect(ngram("foo")).toEqual(["f", "fo", "foo", "o", "oo", "o"]);
+      // sets max as string length and min as 1
+      expect(ngram("foo", { style: NgramStyle.START })).toEqual([
+        "f",
+        "fo",
+        "foo",
+      ]);
+      expect(ngram("foo", { style: NgramStyle.START, max: -1 })).toEqual([
+        "f",
+        "fo",
+        "foo",
+      ]);
+      expect(ngram("foo", { style: NgramStyle.START, max: 25 })).toEqual([
+        "f",
+        "fo",
+        "foo",
+      ]);
+      expect(ngram("foo", { style: NgramStyle.START, min: -1 })).toEqual([
+        "f",
+        "fo",
+        "foo",
+      ]);
+    });
 
-    expect(
-      ngram("opensource", { style: NgramStyle.MIDDLE, min: 3, max: 5 }),
-    ).toEqual([
-      "ope",
-      "open",
-      "opens",
-      "pen",
-      "pens",
-      "penso",
-      "ens",
-      "enso",
-      "ensou",
-    ]);
-    expect(ngram("foo", { style: NgramStyle.MIDDLE, min: 3, max: 5 })).toEqual([
-      "foo",
-    ]);
+    it("should return list of grams", () => {
+      expect(
+        ngram("opensource", { style: NgramStyle.START, min: 3, max: 5 }),
+      ).toEqual(["ope", "open", "opens"]);
+      expect(ngram("foo", { style: NgramStyle.START, min: 3, max: 5 })).toEqual(
+        ["foo"],
+      );
+
+      expect(
+        ngram("opensource", { style: NgramStyle.MIDDLE, min: 3, max: 5 }),
+      ).toEqual([
+        "ope",
+        "open",
+        "opens",
+        "pen",
+        "pens",
+        "penso",
+        "ens",
+        "enso",
+        "ensou",
+      ]);
+      expect(
+        ngram("foo", { style: NgramStyle.MIDDLE, min: 3, max: 5 }),
+      ).toEqual(["foo"]);
+    });
   });
 });
